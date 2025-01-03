5bb535aa8f434d36ee0204d7e4451da660b75b26
Qguy09-03 <jonesquincy994@gmail.com>
2025-01-03 13:42:20 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,7 +43,7 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("silence.mp3"); //load sound
+export var wallSound = new soundFile("Eli_Terry_Skill_01_01.wav"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
 export var paddleSound = new soundFile("hit sound.ogg");
